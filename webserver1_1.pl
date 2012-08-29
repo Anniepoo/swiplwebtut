32b29ce550ac5fd73bf7817182c6bf2fe8fa5b6c
Anniepoo <annie66us@yahoo.com>
2012-08-28 17:48:59 -0700

    experimental checkin

---
+++ webserver1_1.pl
@@ -0,0 +1,25 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+
+% The predicate server(+Port) starts the server. It simply creates a
+% number of Prolog threads and then returns to the toplevel, so you can
+% (re-)load code, debug, etc.
+server(Port) :-
+        http_server(http_dispatch, [port(Port)]).
+
+% Declare a handler, binding an HTTP path to a predicate.
+% Here our path is / (the root) and the goal we'll query will be
+% say_hi. The third argument is for options
+:- http_handler(/, say_hi, []).
+
+/* The implementation of /. The single argument provides the request
+details, which we ignore for now. Our task is to write a CGI-Document:
+a number of name: value -pair lines, followed by two newlines, followed
+by the document content, The only obligatory header line is the
+Content-type: <mime-type> header.
+Printing can be done using any Prolog printing predicate, but the
+format-family is the most useful. See format/2.   */
+
+say_hi(_Request) :-
+        format('Content-type: text/plain~n~n'),
+        format('Hello World!~n').
---
+++ webserver1_3.pl
@@ -0,0 +1,50 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_error)).
+
+%
+%  Add another abstract base location like root
+%  this one is files, and we map it to /f,
+%  so /f/limburger can be described as files(limburger)
+:- multifile http:location/3.
+:- dynamic   http:location/3.
+
+http:location(files, '/f', []).
+
+:- http_handler(files(bunny), say_bunny , []).
+
+% Declare a handler, binding an HTTP path to a predicate.
+% The notation root(hello_world) uses an alias-mechanism similar to
+% absolute_file_name/3 and allows for moving parts of the server locations
+% easily. See http_absolute_location/3.
+:- http_handler(root(.), say_hi, []).
+
+% And, just for clarity, define a second handler
+% this one can by reached at http://127.0.0.1:8000/taco
+:- http_handler(root(taco), say_taco, []).
+
+% The predicate server(?Port) starts the server. It simply creates a
+% number of Prolog threads and then returns to the toplevel, so you can
+% (re-)load code, debug, etc.
+server(Port) :-
+        http_server(http_dispatch, [port(Port)]).
+
+/* The implementation of /hello_world. The single argument provides the request
+details, which we ignore for now. Our task is to write a CGI-Document:
+a number of name: value -pair lines, followed by two newlines, followed
+by the document content, The only obligatory header line is the
+Content-type: <mime-type> header.
+Printing can be done using any Prolog printing predicate, but the
+format-family is the most useful. See format/2.   */
+
+say_hi(_Request) :-
+        format('Content-type: text/plain~n~n'),
+        format('Hello World!~n').
+
+say_taco(_Request) :-
+        format('Content-type: text/plain~n~n'),
+        format('Tacos are yummie!~n').
+
+say_bunny(_Request) :-
+        format('Content-type: text/plain~n~n'),
+        format('bunnies are cute!~n').
